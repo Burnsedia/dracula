206dcf71a09e358927d3925c3784234a351df4f4
Burnsedia <bburnsed1995@gmail.com>
2023-01-25 05:51:14 -0500

    fixed typos

--- src/components/BloodSugarLog.js
+++ src/components/BloodSugarLog.js
@@ -1,7 +1,38 @@
 import { Link } from "gatsby"
 import React, { useState} from "react"
 import styled, { css } from "styled-components"
+import { IonApp, 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonFooter, 
+  IonMenu,
+  IonList,
+  IonListHeader,
+  IonMenuToggle,
+  IonIcon,
+  IonLabel,
+  IonPage ,
+  IonButtons,
+  IonButton,
+  IonItem } from '@ionic/react';
 
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 const Log = styled.div`
   background-color: #ff5555;
   margin: 0 0 25px;
