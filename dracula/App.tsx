e88335443137283138475fa30058d1b799c0e31f
Burnsedia <bburnsed1995@gmail.com>
2023-10-15 20:09:36 -0400

    debugging navbar

--- dracula/App.tsx
+++ dracula/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as NavigationBar from 'expo-navigation-bar';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <NavigationBar style="auto"/>
+      <Text>A Privacy Focused Blood Sugar Tracking app</Text>
       <StatusBar style="auto" />
     </View>
   );
