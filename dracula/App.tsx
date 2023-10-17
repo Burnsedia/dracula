8e0d52f6be8cec5184914f7325cd3fde508dbba3
Burnsedia <bburnsed1995@gmail.com>
2023-10-16 20:50:58 -0400

    Adding componets and pages to orginize the app

--- dracula/App.tsx
+++ dracula/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import * as NavigationBar from 'expo-navigation-bar';
+import { NavigationBar } from 'expo-navigation-bar';
 export default function App() {
   return (
     <View style={styles.container}>
