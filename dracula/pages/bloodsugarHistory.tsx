936acc89ffe790824c9048093a8acbbf5c9ae8c3
Burnsedia <bburnsed1995@gmail.com>
2023-10-30 07:31:41 -0400

    canned background and text color

--- dracula/pages/bloodsugarHistory.tsx
+++ dracula/pages/bloodsugarHistory.tsx
@@ -28,7 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    color: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
