71e696f0d869b3848bfcdee8ea80ebd943d1e7e1
Burnsedia <bburnsed1995@gmail.com>
2025-03-16 18:21:05 -0400

    creating my models for my production version

--- dracula/lib/models/bloodsugar.dart
+++ dracula/lib/models/bloodsugar.dart
@@ -1,7 +1,30 @@
-class Record {
+class BloodSugarLog {
   final double bloodSugar;
-  final int carbs;
-  Record({required this.bloodSugar, required this.carbs});
+  final bool isBeforeMeal;
+  final DateTime createdAt;
+
+
+  BloodSugarLog({
+    required this.bloodSugar,
+    required this.isBeforeMeal,
+    required this.createdAt
+  });
+
+  factory BloodSugarLog.fromJson(Map<String, dynamic> json){
+    return BloodSugarLog(
+      bloodSugar: json['bloodSugar'],
+      isBeforeMeal: json['isBeforeMeal'],
+      createdAt: json['createdAt'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "bloodSugar": bloodSugar,
+      "isBeforeMeal": isBeforeMeal,
+      "createdAt": createdAt,
+    }
+  }
 }
 
 
